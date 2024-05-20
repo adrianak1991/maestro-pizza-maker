2c3681858ac8d6abb2e8b831dda10c4f1a05a2a9
adrianak1991 <88764180+adrianak1991@users.noreply.github.com>
2024-05-20 11:04:29 +0200

    Definition taste at risk functions
    Definition of functions:
    taste_at_risk_pizza,
    taste_at_risk_menu,
    conditional_taste_at_risk_pizza,
    conditional_taste_at_risk_menu

--- maestro_pizza_maker/taste_at_risk.py
+++ maestro_pizza_maker/taste_at_risk.py
@@ -12,24 +12,27 @@ def taste_at_risk_pizza(pizza: Pizza, quantile: float) -> float:
     # quantile is the quantile that we want to consider
     # Hint: Similarity between the Taste at Risk and the Value at Risk is not a coincidence or is it?
     # Hint: Use function taste from Pizza class, but be aware that the higher the taste, the better -> the lower the taste, the worse
-    pass
+    return np.percentile(pizza.taste,quantile*100)
 
 
 def taste_at_risk_menu(menu: PizzaMenu, quantile: float) -> float:
     # TODO: implement the taste at risk measure for a menu
     # quantile is the quantile that we want to consider
     # Hint: the taste of the whole menu is the sum of the taste of all pizzas in the menu, or? ;)
-    pass
+    return np.percentile(sum(pizza.taste for pizza in menu.pizzas),quantile*100)
 
 
 def conditional_taste_at_risk_pizza(pizza: Pizza, quantile: float) -> float:
     # TODO: implement the conditional taste at risk measure for a pizza
     # quantile is the quantile that we want to consider
     # Hint: Simmilarity between the Conditional Taste at Risk and the Conditional Value at Risk is not a coincidence or is it?
-    pass
+    return np.mean(pizza.taste[pizza.taste <=taste_at_risk_pizza(pizza, quantile)])
+    # return np.mean(pizza.taste[pizza.taste <=np.percentile(pizza.taste,quantile*100)])
 
 
 def conditional_taste_at_risk_menu(menu: PizzaMenu, quantile: float) -> float:
     # TODO: implement the conditional taste at risk measure for a menu
     # Hint: the taste of the whole menu is the sum of the taste of all pizzas in the menu, or? ;) (same as for the taste at risk)
-    pass
+    menu_taste = sum(pizza.taste for pizza in menu.pizzas)
+    return np.mean(menu_taste[menu_taste<=taste_at_risk_menu(menu,quantile)])
+    # return np.mean(menu_taste[menu_taste<=np.percentile(menu_taste,quantile*100)])
